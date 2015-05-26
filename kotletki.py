358d04f44e48e625790ad6a0c34c3ff1dbd884bd
Ale <ale@songbee.net>
2015-05-27 00:25:53 +0300

    pam anderson is going to love me.
    http://whatthecommit.com/4b8c0afa9cd2967665844e1788515889

--- kotletki.py
+++ kotletki.py
@@ -1,4 +1,7 @@
 class Kotletka(object):
+    ready_after = 2
+    burnt_after = 5
+
     def __init__(self):
         self.up = True
         self._times_turned = 0
@@ -7,19 +10,27 @@ class Kotletka(object):
         self.up = not self.up
         self._times_turned += 1
 
+        if self._times_turned >= self.burnt_after:
+            raise KotletkaBurntException()
+
     @property
     def ready(self):
         return self._times_turned >= 2
 
 
-class Bun(object):
-    def __init__(self):
-        pass
+class ChickenKotletka(Kotletka):
+    ready_after = 4
 
 
-class Sauce(object):
-    def __init__(self):
-        pass
+class ImpossibleKotletka(Kotletka):
+    ready_after = 8
+    # burnt_after == 5
+
+
+class Bun(object): pass
+
+
+class Sauce(object): pass
 
 
 class Ketchup(Sauce):
@@ -41,15 +52,25 @@ class Skovoroda(object):
 class Burger(list):
     def __init__(self, kotletka, buns, sauce):
         if not kotletka.ready:
-            raise YouSuckAtCookingException
+            raise KotletkaNotReadyException
         self.extend([buns[0], kotletka, sauce, buns[1]])
 
 
 class SkovorodaOverflowException(Exception):
     def __str__(self):
-        return 'Wow such exception'
+        return 'Too many items at skovoroda'
 
 
 class YouSuckAtCookingException(Exception):
     def __str__(self):
         return 'Your chef sucks at cooking'
+
+
+class KotletkaBurntException(YouSuckAtCookingException):
+    def __str__(self):
+        return 'Kotletka was overcooked'
+
+
+class KotletkaNotReadyException(YouSuckAtCookingException):
+    def __str__(self):
+        return "Kotletka wasn't cooked properly"
