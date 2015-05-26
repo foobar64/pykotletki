d8db305c38e0f461f06b9afbe9403e6f89cd1deb
Vladimir Shargin <mail@foobar64.me>
2015-05-26 23:28:26 +0300

    McDonalds Simulator 2015

--- kotletki.py
+++ kotletki.py
@@ -6,7 +6,57 @@ class Kotletka(object):
     def turn_over(self):
         self.up = not self.up
         self._times_turned += 1
-    
+
     @property
     def ready(self):
         return self._times_turned >= 2
+
+
+class Bun(object):
+    def __init__(self):
+        pass
+
+
+class Sauce(object):
+    def __init__(self):
+        pass
+
+
+class Ketchup(Sauce):
+    def __init__(self):
+        Sauce.__init__(self)
+        self.color = 'red'
+
+
+class Skovoroda(object):
+    def __init__(self):
+        self.contents = []
+
+    def put(self, food):
+        if len(self.contents > 5):
+            raise SkovorodaOverflowException
+        self.contents.extend(food)
+
+
+class Burger(object):
+    def __init__(self, kotletka, bun, sauce):
+        self.ingredients = []
+        if not kotletka.ready:
+            raise YouSuckAtCookingException
+        self.ingredients.extend([kotletka, bun, sauce])
+
+
+class SkovorodaOverflowException(Exception):
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return 'Wow such exception'
+
+
+class YouSuckAtCookingException(Exception):
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return 'Your chef sucks at cooking'
